8904c9be24d2159d38bfcba35a4b7911e77a9d46
ufispb <ufispb@gmail.com>
2021-04-15 00:34:56 +0300

    added sprites of hexadecimal digits

--- src/main/java/org/some/chip8/App.java
+++ src/main/java/org/some/chip8/App.java
@@ -1,56 +1,56 @@
 package org.some.chip8;
 
 
-import java.util.Arrays;
 import lombok.SneakyThrows;
 
-public class App
-{
-    public static void main(String[] args) {
-        Chip8VirtualMachine virtualMachine = new Chip8VirtualMachine();
-        virtualMachine.clearBuffer();
+public class App {
 
-        byte one = 0x1;
+  public static void main(String[] args) {
+    Chip8VirtualMachine virtualMachine = new Chip8VirtualMachine();
+    virtualMachine.clearBuffer();
 
-        int width = 8;
-        int height = 5;
+    int width = 10;
+    int height = 15;
 
-        byte[] frameBuffer = new byte[width * height];
-        Arrays.fill(frameBuffer, 0, 4, one);
-        frameBuffer[8] = one;
-        Arrays.fill(frameBuffer, 2 * 8, 2 * 8 + 4, one);
-        frameBuffer[4 * 7 - 1] = one;
-        Arrays.fill(frameBuffer, 4 * 8, 4 * 8 + 4, one);
+    byte[] frameBuffer = new byte[width * height];
+    ConsoleDisplay consoleDisplay = new ConsoleDisplay(width, height);
 
+    byte[][] digits = new byte[][]{Sprites.C, Sprites.TWO, Sprites.D, Sprites.F};
 
-        ConsoleDisplay consoleDisplay = new ConsoleDisplay(width, height);
-
-        int t = 3;
-        while (t-- >= 0) {
-            clearScreen();
-            consoleDisplay.display(frameBuffer);
-        }
+    for (byte[] aDigit : digits) {
+      clearScreen();
+      drawDigit(frameBuffer, width, aDigit);
+      consoleDisplay.display(frameBuffer);
     }
+  }
 
-    @SneakyThrows
-    private static void clearScreen() {
-        Thread.sleep(500);
-        System.out.print("\033[H\033[2J");
-        System.out.flush();
-        Thread.sleep(500);
+  private static void drawDigit(byte[] frameBuffer, int width, byte[] digit) {
+    for (int i = 0; i < digit.length; i++) {
+      frameBuffer[i * width] = (byte) (digit[i] & 8);
+      frameBuffer[i * width + 1] = (byte) (digit[i] & 4);
+      frameBuffer[i * width + 2] = (byte) (digit[i] & 2);
+      frameBuffer[i * width + 3] = (byte) (digit[i] & 1);
     }
+  }
 
-    private static void showChars() {
-        for (int k = 0; k < 7; k++) {
-            for (int i = 20 + 30 * k; i < 20 + 30 * (k + 1); i++) {
-                System.out.printf("%4d", i);
-            }
-            System.out.println();
-            for (int i = 20 + 30 * k; i < 20 + 30 * (k + 1); i++) {
-                System.out.printf("%4c", (char) i);
-            }
-            System.out.println();
-            System.out.println("----------------------------------------");
-        }
+  @SneakyThrows
+  private static void clearScreen() {
+    Thread.sleep(500);
+    System.out.print("\033[H\033[2J");
+    System.out.flush();
+  }
+
+  private static void showChars() {
+    for (int k = 0; k < 7; k++) {
+      for (int i = 20 + 30 * k; i < 20 + 30 * (k + 1); i++) {
+        System.out.printf("%4d", i);
+      }
+      System.out.println();
+      for (int i = 20 + 30 * k; i < 20 + 30 * (k + 1); i++) {
+        System.out.printf("%4c", (char) i);
+      }
+      System.out.println();
+      System.out.println("----------------------------------------");
     }
+  }
 }
